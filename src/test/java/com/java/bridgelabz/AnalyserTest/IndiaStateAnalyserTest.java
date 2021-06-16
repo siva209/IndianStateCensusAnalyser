005c4d7da69f55b5854bc2746f1f678518abbfca
siva209 <sivanarayana209@gmail.com>
2021-06-16 23:33:24 +0530

    Added:test case for Checking No of Records Matches

--- src/test/java/com/java/bridgelabz/AnalyserTest/IndiaStateAnalyserTest.java
+++ src/test/java/com/java/bridgelabz/AnalyserTest/IndiaStateAnalyserTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import com.java.bridgelabz.CensusAnalyser;
 import com.java.bridgelabz.CensusAnalyserException;
 
+
 public class IndiaStateAnalyserTest {
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\admin\\eclipse-workspace\\IndianStateCensusCSV\\src\\main\\resources\\IndiaStateCensusData.csv";
 	private static final String WRONG_FILE_PATH = "C:\\Users\\main\\eclipse-workspace\\IndianStateCensusCSV\\src\\main\\resources\\IndiaStateCensusData.csv";
@@ -17,58 +18,76 @@ public class IndiaStateAnalyserTest {
 	private static final String FILE_PATH_DELIMITER = "C:\\Users\\admin\\eclipse-workspace\\IndianStateCensusCSV\\src\\main\\\resources\\IndiaStateCensusDataDelimiter.csv";
 	private static final String FILE_PATH_HEADER = "C:\\Users\\admin\\eclipse-workspace\\IndianStateCensusCSV\\src\\main\\resources\\IndiaStateCensusDataHeader.csv";
 
+	
+	private static final String FILE_PATH_STATECODE = "C:\\\\Users\\\\admin\\\\eclipse-workspace\\\\IndianStateCensusCSV\\\\src\\\\main\\\\resources\\\\IndiaStateCode.csv";
+	
 	@Test
 	public void givenIndianCensusCSVFileReturnsCorrectRecords() throws IOException, CensusAnalyserException {
+        
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadCSVData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29,numOfRecords);
+        
+        }
+	
 
-		CensusAnalyser censusAnalyser = new CensusAnalyser();
-		int numOfRecords = censusAnalyser.loadCSVData(INDIA_CENSUS_CSV_FILE_PATH);
-		Assert.assertEquals(29, numOfRecords);
+@Test
+public void givenCSVFile_whenWrongFile_ShouldThrowError() throws IOException {
+	CensusAnalyser censusAnalyser = new CensusAnalyser();
+	try {
+		 int numOfRecords = censusAnalyser.loadCSVData(WRONG_FILE_PATH);
+	} catch (CensusAnalyserException exception) {
+		System.out.println("No such file found");
+		assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
+	}
+}
 
+@Test
+public void givenCSVFile_whenFileCorrect_butExtensionIncorrect_shouldThrowError() throws IOException {
+	CensusAnalyser censusAnalyser = new CensusAnalyser();
+	try {
+		 int numOfRecords = censusAnalyser.loadCSVData(FILE_PATH_WRONG_EXTENSION);
+	} catch (CensusAnalyserException exception) {
+		System.out.println("No such file found");
+		assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
+	}
+}
+
+@Test
+public void givenCSVFile_whenFileCorrect_butDelimiterIncorrect_shouldThrowError() throws IOException {
+	CensusAnalyser censusAnalyser = new CensusAnalyser();
+	try {
+		int numOfRecords = censusAnalyser.loadCSVData(FILE_PATH_DELIMITER);
+	} catch (CensusAnalyserException exception) {
+		System.out.println("Incorrect file delimeter");
+		assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, exception.type);
+	}
+}
+
+
+@Test
+public void givenCSVFile_whenFileCorrect_butHeaderIncorrect_shouldThrowError() throws IOException {
+	CensusAnalyser censusAnalyser = new CensusAnalyser();
+	try {
+		int numOfRecords = censusAnalyser.loadCSVData(FILE_PATH_HEADER);
+
+	} catch (CensusAnalyserException exception) {
+		System.out.println("Incorrect file Header");
+		assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, exception.type);
+	}
+}
+
+
+
+@Test
+public void givenIndianStaeCodeCSVFileReturnsCorrectRecords() throws CensusAnalyserException, IOException {
+	  CensusAnalyser censusAnalyser = new CensusAnalyser();
+		int count = censusAnalyser.loadStateCSV(FILE_PATH_STATECODE);
+		System.out.println(count);
+		Assert.assertEquals(37, count);
 	}
 
-	@Test
-	public void givenCSVFile_whenWrongFile_ShouldThrowError() throws IOException {
-		CensusAnalyser censusAnalyser = new CensusAnalyser();
-		try {
-			int numOfRecords = censusAnalyser.loadCSVData(WRONG_FILE_PATH);
-		} catch (CensusAnalyserException exception) {
-			System.out.println("No such file found");
-			assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
-		}
-	}
+       
 
-	@Test
-	public void givenCSVFile_whenFileCorrect_butExtensionIncorrect_shouldThrowError() throws IOException {
-		CensusAnalyser censusAnalyser = new CensusAnalyser();
-		try {
-			int numOfRecords = censusAnalyser.loadCSVData(FILE_PATH_WRONG_EXTENSION);
-		} catch (CensusAnalyserException exception) {
-			System.out.println("No such file found");
-			assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
-		}
-	}
-
-	@Test
-	public void givenCSVFile_whenFileCorrect_butDelimiterIncorrect_shouldThrowError() throws IOException {
-		CensusAnalyser censusAnalyser = new CensusAnalyser();
-		try {
-			int numOfRecords = censusAnalyser.loadCSVData(FILE_PATH_DELIMITER);
-		} catch (CensusAnalyserException exception) {
-			System.out.println("Incorrect file delimeter");
-			assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, exception.type);
-		}
-	}
-
-	@Test
-	public void givenCSVFile_whenFileCorrect_butHeaderIncorrect_shouldThrowError() throws IOException {
-		CensusAnalyser censusAnalyser = new CensusAnalyser();
-		try {
-			int numOfRecords = censusAnalyser.loadCSVData(FILE_PATH_HEADER);
-
-		} catch (CensusAnalyserException exception) {
-			System.out.println("Incorrect file Header");
-			assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, exception.type);
-		}
-	}
 
 }
