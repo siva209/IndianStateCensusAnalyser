9a805a6271a007dd48d7a6d69d6f356751ebfb1d
siva209 <sivanarayana209@gmail.com>
2021-06-16 22:54:56 +0530

    Added:Testcase to check for correct CSV file with wrong extension

--- src/test/java/com/java/bridgelabz/AnalyserTest/IndiaStateAnalyserTest.java
+++ src/test/java/com/java/bridgelabz/AnalyserTest/IndiaStateAnalyserTest.java
@@ -14,6 +14,8 @@ import com.java.bridgelabz.CensusAnalyserException;
 public class IndiaStateAnalyserTest {
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\admin\\eclipse-workspace\\IndianStateCensusCSV\\src\\main\\resources\\IndiaStateCensusData.csv";
 	private static final String WRONG_FILE_PATH = "C:\\Users\\main\\eclipse-workspace\\IndianStateCensusCSV\\src\\main\\resources\\IndiaStateCensusData.csv";
+	private static final String FILE_PATH_WRONG_EXTENSION = "C:\\\\Users\\\\admin\\\\eclipse-workspace\\\\IndianStateCensusCSV\\\\src\\\\main\\\\resources\\\\IndiaStateCensusData.txt";
+	
 	@Test
 	public void givenIndianCensusCSVFileReturnsCorrectRecords() throws IOException, CensusAnalyserException {
         
@@ -29,6 +31,17 @@ public void givenCSVFile_whenWrongFile_ShouldThrowError() throws IOException {
 	CensusAnalyser censusAnalyser = new CensusAnalyser();
 	try {
 		 int numOfRecords = censusAnalyser.loadCSVData(WRONG_FILE_PATH);
+	} catch (CensusAnalyserException exception) {
+		System.out.println("No such file found");
+		assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
+	}
+}
+
+@Test
+public void givenCSVFile_whenFileCorrect_butExtensionIncorrect_shouldThrowError() throws IOException {
+	CensusAnalyser censusAnalyser = new CensusAnalyser();
+	try {
+		 int numOfRecords = censusAnalyser.loadCSVData(FILE_PATH_WRONG_EXTENSION);
 	} catch (CensusAnalyserException exception) {
 		System.out.println("No such file found");
 		assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
